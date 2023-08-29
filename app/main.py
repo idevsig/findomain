b4fc8a3e9e18ef6a03231cad7f3636a21838d5ef
Jetsung Chan <jetsungchan@gmail.com>
2023-08-29 11:59:33 +0800

    功能：支持自定义 Whos ISP

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import random
 from config.config_loader import load_config
 from .utils.request import HttpRequest
 from .notify.feishu import Feishu
@@ -63,6 +64,9 @@ class App:
         }
         self.notify = {**default_notify, **config.get('notify', {})}
 
+        # 初始化 ISP
+        self.init_isp()
+
         setting = config['setting']
         whois = config['whois']
 
@@ -89,6 +93,48 @@ class App:
             # 日志格式
             format='%(message)s'
         )
+
+    '''
+    初始化 ISP
+    '''
+
+    def init_isp(self):
+        # 全部
+        self.isps = [
+            West(),
+            Qcloud(),
+            Zzidc(),
+            Rapidapi()
+        ]
+
+        # 自定义 ISP 商
+        if self.whois['isp']:
+            arr = str(self.whois['isp']).split(',')
+            isps = []
+
+            # 配置中的 ISPS 遍历
+            for name in arr:
+                # ISPS 遍历
+                for isp in self.isps:
+                    # ISP 名转小写
+                    isp_name = str(isp.__class__.__name__).lower()
+                    # 匹配名称
+                    if name == isp_name:
+                        supported = isp.supported(self.domain['suffixes'])
+                        # ISP 不支持此后缀
+                        if not supported:
+                            print(
+                                f'{isp_name} suffixes supported: {supported}')
+                        else:  # 支持的 ISP
+                            isps.append(isp)
+                        # 数据已匹配，跳出此循环
+                        break
+            # 若存在自定义 ISP，则更新
+            if len(isps) > 0:
+                self.isps = isps
+
+        # 获取 ISP 数量
+        self.isp_count = len(self.isps)
 
     '''
     生成域名列表
@@ -165,7 +211,6 @@ class App:
     '''
 
     def transfer_file(self, log_path):
-        print(self.setting['transfer'])
         if self.setting['transfer']:
             try:
                 req = HttpRequest()
@@ -182,23 +227,63 @@ class App:
                 print(f'transfer_file err: {e}')
                 return None
 
+    '''
+    随机 ISP
+    '''
+
+    def random_isp(self):
+        idx = random.randint(0, len(self.isps) - 1)
+        return self.isps[idx]
+
+    def fetch(self):
+        count = 0
+
+        for domain in self.domains:
+            # print(domain)
+            retries = 0  # 重试次数计数器
+            max_retries = 10  # 最多重试10次
+            should_break = False  # 标志，用于控制是否跳出最外层的for循环
+
+            while retries < max_retries:
+                try:
+                    isp = self.random_isp()
+                    isp_name = isp.__class__.__name__
+                    available, regdate, expdate, err = isp.available(domain)
+
+                    # 打印日志
+                    print(f'{domain}, {available}, {regdate}, {expdate}, {err}')
+
+                    # whois 查询失败
+                    if err != 0:
+                        raise ValueError(
+                            f'{isp_name} fetch domain {domain} failed.')
+
+                    logging.info(
+                        f'{domain}, {1 if available else 0 }, {err}, {regdate}, {expdate}')
+                    count += 1  # 增加成功数
+                    break
+                except Exception as e:
+                    print(f'transfer_file err: {e}')
+                    retries += 1  # 增加重试次数
+                    if retries >= max_retries:
+                        print("Max retries reached. Exiting loop.")
+                        should_break = True  # 设置标志以跳出最外层的for循环
+                        break  # 达到最大重试次数，退出循环
+                    continue
+
+            if should_break:
+                break  # 如果标志被设置，跳出最外层的for循环
+        if count == 0:
+            die('Failed to retrieve valid domain data.')
+
     def run(self):
         print("Domain:", self.domain)
         print("Notify:", self.notify)
         print("\n")
 
         # print(self.domains)
+        self.fetch()
 
-        isp = Zzidc()
-        supported = isp.supported(self.domain['suffixes'])
-        print(f'supported: {supported}')
-        for domain in self.domains:
-            print(domain)
-            res = isp.available(domain)
-            print(res)
-            break
-
-        logging.info(datetime.now())
         log_path = self.setting['log_path']
         transfer_url = self.transfer_file(log_path)
         message = self.read_log(log_path, transfer_url)
